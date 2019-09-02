db8c28d36b089e8b819da7575a10e9b8e5e23127
Leonel Menezes <mml_leo@hotmail.com>
2019-09-02 20:45:10 -0300

    first commit with project

---
+++ PlusUltraQuiz/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PlusUltraQuiz
+//
+//  Created by Leonel Menezes on 02/09/19.
+//  Copyright © 2019 Leonel Menezes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UIViewController()
+        return true
+    }
+}
+
---
+++ PlusUltraQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlusUltraQuiz
+//
+//  Created by Leonel Menezes on 02/09/19.
+//  Copyright © 2019 Leonel Menezes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
