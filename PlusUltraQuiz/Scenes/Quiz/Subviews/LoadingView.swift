bb96fafa8b0b0d6398d19a724d22f5667a1bbe72
Leonel Menezes <mml_leo@hotmail.com>
2019-09-03 20:35:44 -0300

    Creates Loading view

--- PlusUltraQuiz/Scenes/Quiz/Subviews/LoadingView.swift
+++ PlusUltraQuiz/Scenes/Quiz/Subviews/LoadingView.swift
@@ -6,4 +6,59 @@
 //  Copyright © 2019 Leonel Menezes. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class LoadingView: UIView {
+    private var indicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.style = .white
+        return indicator
+    }()
+    
+    private var loadingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    init(frame: CGRect = .zero, alpha: CGFloat) {
+        super.init(frame: frame)
+        setupViews()
+        self.alpha = alpha
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func animate() {
+        indicator.startAnimating()
+    }
+}
+
+extension LoadingView: ViewCoding {
+    func buildViewHierarchy() {
+        [indicator, loadingLabel].forEach({addSubview($0)})
+    }
+    
+    func setupConstraints() {
+        let stack = createVerticalStack()
+        addSubview(stack)
+        stack.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+    
+    private func createVerticalStack() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        [indicator, loadingLabel].forEach({stack.addArrangedSubview($0)})
+        return stack
+    }
+    
+    func additionalSettings() {
+        self.backgroundColor = .black
+        self.layer.cornerRadius = 10
+    }
+}
---
+++ PlusUltraQuizTests/Scenes/Quiz/Subviews/LoadingViewSpec.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewSpec.swift
+//  PlusUltraQuizTests
+//
+//  Created by Leonel Menezes on 03/09/19.
+//  Copyright © 2019 Leonel Menezes. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import PlusUltraQuiz
+
+final class LoadingViewSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
